12096d6b177643ffad0fb3a8c641cedff4cd9aad
Bobby Hewitt <bobby.hewitt@w12studios.com>
2019-02-28 13:46:19 +0000

    updates qr, game over etc

--- server/index.js
+++ server/index.js
@@ -14,6 +14,7 @@ app.use( bodyParser.json());       // to support JSON-encoded bodies
 app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
   extended: true
 }))
+app.use(express.static('build'));
 // set up database connection
 
 
